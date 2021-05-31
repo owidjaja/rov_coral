4e899b928b46af05f2477208099b0d61bb16127b
owidjaja <owidjaja@connect.ust.hk>
2021-06-01 03:10:41 +0800

    Fix corner_joints.py
    allow reset of green circles in corner_joints.py
    getStructuringElement allows us to make kernels with irregular shapes
    
    TODO: figure out why div by 0

--- scripts/intersections/corner_joints.py
+++ scripts/intersections/corner_joints.py
@@ -17,22 +17,22 @@ thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 cv2.imshow('thresh', thresh)
 
 cv2.namedWindow("Trackbar_Window", cv2.WINDOW_NORMAL)
-cv2.createTrackbar("ksize1", "Trackbar_Window", 10, 30, cb_nothing)
+cv2.createTrackbar("ksize1", "Trackbar_Window", 10, 50, cb_nothing)
 cv2.createTrackbar("ksize2", "Trackbar_Window", 1, 10, cb_nothing)
 
 while True:
     ksize1 = cv2.getTrackbarPos("ksize1", "Trackbar_Window")
     if ksize1<=0: ksize1 = 1
     ksize2 = cv2.getTrackbarPos("ksize2", "Trackbar_Window")
-    if ksize2>=0: ksize2 = 1
+    if ksize2<=0: ksize2 = 1
 
     # Find horizonal lines
-    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (ksize1,ksize2))
+    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (ksize1,ksize2))  # rectangular kernel
     horizontal = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
     cv2.imshow('horizontal', horizontal)
 
     # Find vertical lines
-    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (ksize2,ksize1))
+    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (ksize2,ksize1))    # vertical rectangular kernel
     vertical = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, vertical_kernel, iterations=2)
     cv2.imshow('vertical', vertical)
 
@@ -40,21 +40,24 @@ while True:
     joints = cv2.bitwise_and(horizontal, vertical)
     cv2.imshow('joints', joints)
 
+    image2 = image.copy()       # to reset green circles
+
     # Find centroid of the joints
     cnts = cv2.findContours(joints, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]       # get contour return value, instead of ret
     for c in cnts:
         # Find centroid and draw center point
         M = cv2.moments(c)
         # print(M,"\n")
         if (M['m00']==0):
+            # TODO: figure out what happens here
             # print("div by 0")
             continue
         cx = int(M['m10']/M['m00'])
         cy = int(M['m01']/M['m00'])
-        cv2.circle(image, (cx, cy), 3, (36,255,12), -1)\
+        cv2.circle(image2, (cx, cy), 3, (36,255,12), -1)
 
-    cv2.imshow('image', image)
+    cv2.imshow('image2', image2)
 
-    if cv2.waitKey(100) == 27:
+    if cv2.waitKey(1) == 27:    # esc to break
         break
