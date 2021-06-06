c5af68fe4d0c9783955e2669971c6fb0e9bae977
owidjaja <owidjaja@connect.ust.hk>
2021-06-07 03:47:00 +0800

    Add all 4 changes to all_diff.py
    succesful drawing changes in colors in canvas
    but need to work on corresponding rectangle colors
    
    also need to replace new_coral = cv2.imread("cropped.jpg")
    also need to look into overlapping contours

--- scripts/albert/all_diff.py
+++ scripts/albert/all_diff.py
@@ -3,10 +3,10 @@ import numpy as np
 from datetime import datetime
 startTime = datetime.now()
 
-old_pink = cv2.imread("eyedrop_pink_mask.JPG")
-old_white = cv2.imread("eyedrop_white_mask.jpg")
-new_pink = cv2.imread("new_pink_mask.jpg")
-new_white = cv2.imread("new_white_mask.jpg")
+new_pink = cv2.imread("eyedrop_pink_mask.JPG")
+new_white = cv2.imread("eyedrop_white_mask.jpg")
+old_pink = cv2.imread("new_pink_mask.jpg")
+old_white = cv2.imread("new_white_mask.jpg")
 
 def get_nonzero(img, to_open=False, ksize=3):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -54,83 +54,127 @@ MAX_DIST = 30
 
 beforeloop = datetime.now()
 print("time to bef loop:", beforeloop - startTime)
-for coor in newpink_nonzero:
-    inloop = datetime.now()
 
-    target = coor[0]
-    tar_x, tar_y = target[0], target[1]
+def process_changes(this_nonzero, other_pink_nonzero, other_white_nonzero, ispink, color1, color2, max_dist=MAX_DIST):
+    for coor in this_nonzero:
+        target = coor[0]
+        tar_x, tar_y = target[0], target[1]
 
-    # new_pink_copy = new_pink.copy()
-    # cv2.circle(new_pink_copy, (tar_x, tar_y), 5, (0,0,255), -1)
-    # cv2.imshow("ITER", new_pink_copy)
+        other_pink_distances = np.sqrt((other_pink_nonzero[:,:,0] - tar_x) ** 2 + (other_pink_nonzero[:,:,1] - tar_y) ** 2)
+        nearest_index = np.argmin(other_pink_distances)
+        pink_distance = other_pink_distances[nearest_index][0]
+        
+        other_white_distances = np.sqrt((other_white_nonzero[:,:,0] - tar_x) ** 2 + (other_white_nonzero[:,:,1] - tar_y) ** 2)
+        nearest_index = np.argmin(other_white_distances)
+        white_distance = other_white_distances[nearest_index][0]
 
-    befdist = datetime.now()
-    # print("before disatnce:", befdist - inloop)
+        if white_distance > max_dist and pink_distance > max_dist:
+            """ GROWTH (GREEN) OR DAMAGE (YELLOW) """
+            canvas[tar_y, tar_x] = color1
+            # cv2.circle(canvas, (tar_x, tar_y), 1, color1, 1)
+            # canvas.itemset((tar_y, tar_x, TODO), 255)
 
-    oldpink_distances = np.sqrt((oldpink_nonzero[:,:,0] - tar_x) ** 2 + (oldpink_nonzero[:,:,1] - tar_y) ** 2)
-    nearest_index = np.argmin(oldpink_distances)
-    pink_distance = oldpink_distances[nearest_index][0]
+        elif ispink==False and pink_distance < white_distance:
+            """ BLEACHING (RED) """
+            canvas[tar_y, tar_x] = color2
+            # cv2.circle(canvas, (tar_x, tar_y), 1, color2, 1)
+            # canvas.itemset((tar_y, tar_x, TODO), 255)
+            
+        elif ispink==True and white_distance < pink_distance:
+            """ RECOVERY (BLUE) """
+            canvas[tar_y, tar_x] = color2
+
+        cv2.imshow("canvas", canvas)
+
+        # if cv2.waitKey(1) == 27:
+        #     break
+
+process_changes(newpink_nonzero , oldpink_nonzero, oldwhite_nonzero, ispink=True , color1=(0,255,0)  , color2=(255,0,0), max_dist=MAX_DIST)
+process_changes(newwhite_nonzero, oldpink_nonzero, oldwhite_nonzero, ispink=False, color1=(0,255,0)  , color2=(0,0,255), max_dist=MAX_DIST)
+process_changes(oldpink_nonzero , newpink_nonzero, newwhite_nonzero, ispink=True , color1=(0,255,255), color2=(0,0,255), max_dist=MAX_DIST)
+process_changes(oldwhite_nonzero, newpink_nonzero, newwhite_nonzero, ispink=False, color1=(0,255,255), color2=(255,0,0), max_dist=MAX_DIST)
+
+"""
+# for coor in newpink_nonzero:
+#     inloop = datetime.now()
+
+#     target = coor[0]
+#     tar_x, tar_y = target[0], target[1]
+
+#     # new_pink_copy = new_pink.copy()
+#     # cv2.circle(new_pink_copy, (tar_x, tar_y), 5, (0,0,255), -1)
+#     # cv2.imshow("ITER", new_pink_copy)
+
+#     befdist = datetime.now()
+#     # print("before disatnce:", befdist - inloop)
+
+#     oldpink_distances = np.sqrt((oldpink_nonzero[:,:,0] - tar_x) ** 2 + (oldpink_nonzero[:,:,1] - tar_y) ** 2)
+#     nearest_index = np.argmin(oldpink_distances)
+#     pink_distance = oldpink_distances[nearest_index][0]
     
-    oldwhite_distances = np.sqrt((oldwhite_nonzero[:,:,0] - tar_x) ** 2 + (oldwhite_nonzero[:,:,1] - tar_y) ** 2)
-    nearest_index = np.argmin(oldwhite_distances)
-    white_distance = oldwhite_distances[nearest_index][0]
+#     oldwhite_distances = np.sqrt((oldwhite_nonzero[:,:,0] - tar_x) ** 2 + (oldwhite_nonzero[:,:,1] - tar_y) ** 2)
+#     nearest_index = np.argmin(oldwhite_distances)
+#     white_distance = oldwhite_distances[nearest_index][0]
     
-    aftdist = datetime.now()
-    # print("aft dsit:", aftdist - befdist)
+#     aftdist = datetime.now()
+#     # print("aft dsit:", aftdist - befdist)
 
-    if white_distance > MAX_DIST and pink_distance > MAX_DIST:
-        """ GROWTH (GREEN) """
-        # canvas[y, x] = 255
-        # cv2.circle(canvas, (tar_x, tar_y), 5, (0,255,0), 1)
-        canvas.itemset((tar_y, tar_x, 1), 255)
-    elif white_distance < pink_distance:
-        """ RECOVERY (BLUE) """
-        # cv2.circle(canvas, (tar_x, tar_y), 5, (255,0,0), 1)
-        canvas.itemset((tar_y, tar_x, 0), 255)
-    else:
-        pass
+#     if white_distance > MAX_DIST and pink_distance > MAX_DIST:
+#         """ """ GROWTH (GREEN) """ """
+#         # canvas[y, x] = 255
+#         # cv2.circle(canvas, (tar_x, tar_y), 5, (0,255,0), 1)
+#         canvas.itemset((tar_y, tar_x, 1), 255)
+#     elif white_distance < pink_distance:
+#         """ """ RECOVERY (BLUE) """ """
+#         # cv2.circle(canvas, (tar_x, tar_y), 5, (255,0,0), 1)
+#         canvas.itemset((tar_y, tar_x, 0), 255)
+#     else:
+#         pass
 
-    cv2.imshow("canvas", canvas)
+#     cv2.imshow("canvas", canvas)
 
-    # ref_pink_copy = old_pink.copy()
-    # cv2.circle(ref_pink_copy, (tar_x, tar_y), 5, (255,0,0), -1)
-    # cv2.imshow("ref_pink_copy", ref_pink_copy)
+#     # ref_pink_copy = old_pink.copy()
+#     # cv2.circle(ref_pink_copy, (tar_x, tar_y), 5, (255,0,0), -1)
+#     # cv2.imshow("ref_pink_copy", ref_pink_copy)
 
-    # if cv2.waitKey(1) == 27:
-    #     break
+#     # if cv2.waitKey(1) == 27:
+#     #     break
 
-    befexit = datetime.now()
-    # print("misc after dist:", befexit - aftdist)
-    # print("total inloop:", befexit - inloop)
-    # exit()
+#     befexit = datetime.now()
+#     # print("misc after dist:", befexit - aftdist)
+#     # print("total inloop:", befexit - inloop)
+#     # exit()
 
-for coor in newwhite_nonzero:
-    target = coor[0]
-    tar_x, tar_y = target[0], target[1]
 
-    oldpink_distances = np.sqrt((oldpink_nonzero[:,:,0] - tar_x) ** 2 + (oldpink_nonzero[:,:,1] - tar_y) ** 2)
-    nearest_index = np.argmin(oldpink_distances)
-    pink_distance = oldpink_distances[nearest_index][0]
+# for coor in newwhite_nonzero:
+#     target = coor[0]
+#     tar_x, tar_y = target[0], target[1]
+
+#     oldpink_distances = np.sqrt((oldpink_nonzero[:,:,0] - tar_x) ** 2 + (oldpink_nonzero[:,:,1] - tar_y) ** 2)
+#     nearest_index = np.argmin(oldpink_distances)
+#     pink_distance = oldpink_distances[nearest_index][0]
     
-    oldwhite_distances = np.sqrt((oldwhite_nonzero[:,:,0] - tar_x) ** 2 + (oldwhite_nonzero[:,:,1] - tar_y) ** 2)
-    nearest_index = np.argmin(oldwhite_distances)
-    white_distance = oldwhite_distances[nearest_index][0]
+#     oldwhite_distances = np.sqrt((oldwhite_nonzero[:,:,0] - tar_x) ** 2 + (oldwhite_nonzero[:,:,1] - tar_y) ** 2)
+#     nearest_index = np.argmin(oldwhite_distances)
+#     white_distance = oldwhite_distances[nearest_index][0]
 
-    if white_distance > MAX_DIST and pink_distance > MAX_DIST:
-        """ GROWTH (GREEN) """
-        # cv2.circle(canvas, (tar_x, tar_y), 5, (0,255,0), 1)
-        canvas.itemset((tar_y, tar_x, 1), 255)
-    elif pink_distance < white_distance:
-        """ BLEACHING (RED) """
-        # cv2.circle(canvas, (tar_x, tar_y), 5, (0,0,255), 1)
-        canvas.itemset((tar_y, tar_x, 2), 255)
-    else:
-        pass
+#     if white_distance > MAX_DIST and pink_distance > MAX_DIST:
+#         """ """ GROWTH (GREEN) """ """
+#         # cv2.circle(canvas, (tar_x, tar_y), 5, (0,255,0), 1)
+#         canvas.itemset((tar_y, tar_x, 1), 255)
+#     elif pink_distance < white_distance:
+#         """ """ BLEACHING (RED) """ """
+#         # cv2.circle(canvas, (tar_x, tar_y), 5, (0,0,255), 1)
+#         canvas.itemset((tar_y, tar_x, 2), 255)
+#     else:
+#         pass
 
-    cv2.imshow("canvas", canvas)
+#     cv2.imshow("canvas", canvas)
 
-    # if cv2.waitKey(1) == 27:
-    #     break
+#     # if cv2.waitKey(1) == 27:
+#     #     break
+"""
+
 
 
 afterloop = datetime.now()
@@ -155,9 +199,9 @@ canvas_mask = np.zeros(canvas_gray.shape, np.uint8) # only need grayscale
 MIN_AREA = 200
 for cont in contours:
     area = cv2.contourArea(cont)
-    # print(area)
     if area < MIN_AREA:
         continue
+    print("area:", area)
 
     cv2.drawContours(canvas_mask, cont, -1, 255, -1)
     cont_x, cont_y, cont_w, cont_h = cv2.boundingRect(cont)
