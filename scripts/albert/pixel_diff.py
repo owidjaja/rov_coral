57d96e91b971eb71cb3ecedf1cc324730d411896
owidjaja <owidjaja@connect.ust.hk>
2021-06-07 00:39:41 +0800

    Add time measurement, failed attempt to use local nonzero

--- scripts/albert/pixel_diff.py
+++ scripts/albert/pixel_diff.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from datetime import datetime
+startTime = datetime.now()
 
 def find_nearest_white(nonzero, target):
     distances = np.sqrt((nonzero[:,:,0] - target[0]) ** 2 + (nonzero[:,:,1] - target[1]) ** 2)
@@ -34,10 +36,12 @@ ref_nonzero = cv2.findNonZero(thresh)
 height, width = new_pink.shape[:2]
 canvas = np.zeros((height, width, 3), dtype=np.uint8)
 
+beforeloop = datetime.now()
+print("time to bef loop:", beforeloop - startTime)
 for coor in new_nonzero:
     new_pink_copy = new_pink.copy()
-    cv2.circle(new_pink_copy, (coor[0][0], coor[0][1]), 5, (0,0,255), -1)
-    cv2.imshow("ITER", new_pink_copy)
+    # cv2.circle(new_pink_copy, (coor[0][0], coor[0][1]), 5, (0,0,255), -1)
+    # cv2.imshow("ITER", new_pink_copy)
     
     # nearest_px = find_nearest_white(ref_nonzero, coor[0])
     
@@ -45,7 +49,18 @@ for coor in new_nonzero:
     x, y = target[0], target[1]
     # print("target:", target)
 
+    # lower_x = x - 30
+    # upper_x = x + 30
+    # lower_y = y - 30
+    # upper_y = y + 30
+
+    # if lower_x < 0: x = 0
+    # if lower_y < 0: y = 0
+    # if upper_x > width:  upper_x = width
+    # if upper_y > height: upper_x = width
+
     distances = np.sqrt((ref_nonzero[:,:,0] - x) ** 2 + (ref_nonzero[:,:,1] - y) ** 2)
+    # print("len(dist):", len(distances))
     nearest_index = np.argmin(distances)
     min_distance = distances[nearest_index][0]
     # print("min_distance", min_distance)
@@ -53,18 +68,21 @@ for coor in new_nonzero:
     if min_distance > 30:
         # print("enter draw canvas with min distance:", min_distance)
         # canvas[y, x] = 255
-        cv2.circle(canvas, (x,y), 5, (0,255,0), 1)
-        cv2.imshow("canvas", canvas)
+        # cv2.circle(canvas, (x,y), 5, (0,255,0), 1)
+        canvas.itemset((y,x,1),255)
+        # cv2.imshow("canvas", canvas)
 
     ref_pink_copy = old_pink.copy()
-    cv2.circle(ref_pink_copy, (x, y), 5, (255,0,0), -1)
-    cv2.imshow("ref_pink_copy", ref_pink_copy)
+    # cv2.circle(ref_pink_copy, (x, y), 5, (255,0,0), -1)
+    # cv2.imshow("ref_pink_copy", ref_pink_copy)
 
     # if cv2.waitKey(1) == 27:
     #     break
-    
+afterloop = datetime.now()
+print("time in loop:", afterloop - beforeloop)
+
 cv2.imshow("canvas", canvas)
-print("done")
+# print("done")
 
 canvas = cv2.cvtColor(canvas, cv2.COLOR_BGR2GRAY)
 contours, hier = cv2.findContours(canvas, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -75,6 +93,10 @@ new_coral = cv2.imread("cropped.jpg")
 cv2.rectangle(new_coral, (cont_x-10, cont_y), (cont_x+cont_w, cont_y+cont_h+35), (0,255,0), 5)
 cv2.imshow("new_coral", new_coral)
 
+endtime = datetime.now()
+print("misc after loop:", endtime - afterloop)
+print("total time:", endtime - startTime)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
@@ -83,6 +105,6 @@ plt.subplot(132), plt.imshow(cv2.cvtColor(new_src, cv2.COLOR_BGR2RGB)), plt.titl
 plt.subplot(133), plt.imshow(cv2.cvtColor(new_coral, cv2.COLOR_BGR2RGB)), plt.title("Changes Detected")
 
 # plt.tight_layout()
-plt.show()
+# plt.show()
 
 print("end")
